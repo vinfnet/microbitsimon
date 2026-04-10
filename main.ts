e75b06d901ad9339dce2a8cfe1e2f6522ba981a3
Simon Gallagher <29166491+vinfnet@users.noreply.github.com>
2026-04-10 11:31:53 +0100

    initial config - press button B and it uses the radio to transmit a string

--- main.ts
+++ main.ts
@@ -1,6 +1,29 @@
+input.onPinPressed(TouchPin.P0, function () {
+    music.play(music.createSoundExpression(
+    WaveShape.Noise,
+    5000,
+    0,
+    255,
+    0,
+    500,
+    SoundExpressionEffect.None,
+    InterpolationCurve.Linear
+    ), music.PlaybackMode.UntilDone)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(6)
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString("Hello!")
+})
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(7)
+    radio.setGroup(0)
+    radio.sendString("Hello")
 })
