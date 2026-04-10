d7e3204b33b37503319ee84c6c5aca4b39ca31af
Simon Gallagher <29166491+vinfnet@users.noreply.github.com>
2026-04-10 11:18:17 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+def on_button_pressed_a():
+    basic.show_number(6)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    basic.show_number(7)
+input.on_button_pressed(Button.B, on_button_pressed_b)
---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(6)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(7)
+})
